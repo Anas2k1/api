fc222daa7ddc68473f3f58b7c366f289783745a1
Anas2k1 <112103757+Anas2k1@users.noreply.github.com>
2025-08-06 15:07:32 +0600

    countries added from api

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# React Countries Cards App
+
+This project displays three country cards in a row, each showing a country's name and flag, using data from the [REST Countries API](https://restcountries.com/v3.1/all).
+
+## How to Run
+
+1. Install dependencies:
+   ```sh
+   npm install
+   ```
+2. Start the development server:
+   ```sh
+   npm run dev
+   ```
+3. Open the app in your browser at the URL shown in the terminal (usually http://localhost:5173).
+
+## Features
+- Fetches country data from the REST Countries API
+- Shows three cards in a row with country name and flag
+
+---
+This project was bootstrapped with [Vite](https://vitejs.dev/) and uses React.
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,flags')
+      .then(res => res.json())
+      .then(data => setCountries(data));
+  }, []);
+
+  return (
+    
+    <div>
+      
+      <h1>Country List</h1>
+      <div className="country-row">
+        {countries.map((country, idx) => (
+          <div className="country-card" key={idx}>
+            <img src={country.flags.svg} alt={country.name.common} className="country-flag" />
+            <div>{country.name.common}</div>
+            <button>Visited</button>
+            <button>Wishlist</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
